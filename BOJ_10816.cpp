bd478312bf6a7c318a3c72b7fa8f0249a5d4cb01
Quinsie <81253154+Quinsie@users.noreply.github.com>
2022-03-31 01:06:01 +0900

    Create BOJ_10816.cpp

---
+++ BOJ_10816.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int card, temp, output;
+    unordered_map <int, int> table;
+
+    cin >> card;
+    for (int i = 0; i < card; i++) {
+        cin >> temp;
+        table[temp]++;
+    }
+    cin >> output;
+    for (int i = 0; i < output; i++) {
+        cin >> temp;
+        if (table[temp]) cout << table[temp] << ' ';
+        else cout << 0 << ' ';
+    }
+    
+    return 0;
+}
